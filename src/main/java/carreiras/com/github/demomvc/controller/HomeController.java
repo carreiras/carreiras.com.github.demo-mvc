f7f92fd981d6630c4ef3192afc9aea5e80cd3d63
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-12-15 11:19:56 -0300

    A entidade Endereco

--- src/main/java/carreiras/com/github/demomvc/controller/HomeController.java
+++ src/main/java/carreiras/com/github/demomvc/controller/HomeController.java
@@ -2,7 +2,6 @@ package carreiras.com.github.demomvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
